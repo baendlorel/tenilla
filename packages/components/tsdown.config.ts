98ad89e4c281a352384ed09c751e83e76a8e3d2a
aldia_mechdream <futami16237@gmail.com>
2026-08-01 17:33:20 +0800

    初步添加了一个tsdown文件

---
+++ packages/components/tsdown.config.ts
@@ -0,0 +1,33 @@
+import { readdirSync } from 'node:fs';
+import { join } from 'node:path';
+import { defineConfig } from 'tsdown';
+
+// find all component directories
+function findComponentDirs(): Record<string, string> {
+  const src = join(import.meta.dirname, 'src');
+  const entries: Record<string, string> = {};
+  readdirSync(src, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .forEach(({ name }) => {
+      entries[`${name}/${name}`] = join(src, name, name + '.ts');
+    });
+  return entries;
+}
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    // 'Modal/Modal': 'src/Modal/Modal.ts',
+    // 'Pagination/Pagination': 'src/Pagination/Pagination.ts',
+    // 'Tooltip/Tooltip': 'src/Tooltip/Tooltip.ts',
+    // 'SmartForm/SmartForm': 'src/SmartForm/SmartForm.ts',
+    // 'TabPanel/TabPanel': 'src/TabPanel/TabPanel.ts',
+    ...findComponentDirs(),
+  },
+  outDir: 'dist',
+  format: 'esm',
+  dts: true,
+  css: { splitting: true, minify: true },
+  external: ['@tenilla/core'],
+  clean: true,
+});
