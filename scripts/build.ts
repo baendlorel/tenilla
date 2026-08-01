1710a10f1ddf44f4bd63ed0da20c9191c1a3a46c
aldia_mechdream <futami16237@gmail.com>
2026-08-01 12:43:30 +0800

    feat: 现在会清除增量更新

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,6 @@
 import { execSync } from 'node:child_process';
 import { cpSync, existsSync, readdirSync, readFileSync, rmSync } from 'node:fs';
-import { join } from 'node:path';
+import { join, relative } from 'node:path';
 
 function loadTsConfig(dir: string): { compilerOptions: { outDir: string } } {
   const tsconfigPath = join(dir, 'tsconfig.json');
@@ -17,20 +17,20 @@ function copyCss(srcDir: string, outDir: string) {
     .forEach((d) => {
       const src = join(srcDir, d.name, `${d.name}.css`);
       const out = join(outDir, d.name, `${d.name}.css`);
-      console.log(`Copying CSS from ${src} to ${out}`);
+      console.log(`Copying CSS from ${relative(srcDir, src)} to ${relative(outDir, out)}`);
       cpSync(src, out, { force: true });
     });
 }
 
 export function build(who: string, dir: string) {
   const outDir = loadTsConfig(dir).compilerOptions.outDir;
+  console.log('Removing output directory:', outDir);
   rmSync(join(dir, outDir), { recursive: true, force: true });
-  console.log(`Built ${who} in ${dir}`);
+  console.log('Removing tsbuildinfo file');
+  rmSync(join(dir, 'tsconfig.tsbuildinfo'), { recursive: true, force: true });
 
-  execSync(`pnpm build`, {
-    stdio: 'inherit',
-    cwd: dir,
-  });
+  console.log(`Building ${who} in ${dir}`);
+  execSync(`pnpm build`, { stdio: 'inherit', cwd: dir });
 
   if (who === 'components') {
     copyCss(join(dir, 'src'), join(dir, outDir));
