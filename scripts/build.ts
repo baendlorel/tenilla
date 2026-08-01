7ca38e2e8d903ace232464e954d2bb99befb3d4e
aldia_mechdream <futami16237@gmail.com>
2026-08-01 10:23:11 +0800

    chore 完成build脚本

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,3 +1,22 @@
-import path from 'node:path';
+import { execSync } from 'node:child_process';
+import { existsSync, readFileSync, rmSync } from 'node:fs';
+import { join } from 'node:path';
 
-export function build(who: string, dir: string) {}
+function loadTsConfig(dir: string): { compilerOptions: { outDir: string } } {
+  const tsconfigPath = join(dir, 'tsconfig.json');
+  if (!existsSync(tsconfigPath)) {
+    throw new Error(`tsconfig.json not found in ${dir}`);
+  }
+  const tsconfigContent = readFileSync(tsconfigPath, 'utf-8');
+  return new Function('return ' + tsconfigContent)();
+}
+
+export function build(who: string, dir: string) {
+  const outDir = loadTsConfig(dir).compilerOptions.outDir;
+  rmSync(join(dir, outDir), { recursive: true, force: true });
+  console.log(`Built ${who} in ${dir}`);
+  execSync(`npx tsc`, {
+    stdio: 'inherit',
+    cwd: dir,
+  });
+}
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,11 +1,18 @@
 import { existsSync } from 'node:fs';
 import path from 'node:path';
+import { build } from './build.js';
 
 function main() {
-  const [, , who] = process.argv;
+  const [, , cmd, who] = process.argv;
   const dir = path.join(import.meta.dirname, '..', 'packages', who);
   if (!who || !existsSync(dir)) {
     console.error(`Cannot find package "${who}" or "${dir}"`);
     process.exit(1);
   }
+
+  if (cmd === 'build') {
+    build(who, dir);
+  }
 }
+
+main();
