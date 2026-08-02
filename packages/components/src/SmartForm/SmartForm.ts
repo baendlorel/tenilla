b171f853a86af1b73f22cb43b61d13cf4109156e
aldia_mechdream <futami16237@gmail.com>
2026-08-02 21:20:24 +0800

    feat: 修正了select模式的组件

--- packages/components/src/SmartForm/SmartForm.ts
+++ packages/components/src/SmartForm/SmartForm.ts
@@ -1,15 +1,15 @@
-import { div, h } from '@tenilla/core';
-import { input, button } from '../h-alias.js';
+import { div, h, option } from '@tenilla/core';
+import { input, label } from '../h-alias.js';
 import { row, col, type GridColSpan } from '../Grid/Grid.js';
 import './SmartForm.css';
 
-interface ValueMap {
+interface FormValueMap {
   textarea: string;
   string: string;
   number: number;
   boolean: boolean;
-  'string-array': string[];
-  'number-array': number[];
+  checkboxes: string[];
+  radios: any;
   select: any;
 }
 
@@ -29,16 +29,11 @@ interface FormRow {
   row: Array<FormEntry | FormEntryTextArea | FormEntrySelect>;
 }
 
-interface FormEntry<
-  T extends 'string' | 'number' | 'boolean' | 'string-array' | 'number-array' =
-    | 'string'
-    | 'number'
-    | 'boolean'
-    | 'string-array'
-    | 'number-array',
-> extends EntryBase {
+type NormalFormType = 'string' | 'number' | 'boolean' | 'checkboxes' | 'radios';
+
+interface FormEntry<T extends NormalFormType = NormalFormType> extends EntryBase {
   type: T;
-  value?: ValueMap[T];
+  value?: FormValueMap[T];
 }
 
 interface FormEntryTextArea extends EntryBase {
@@ -53,9 +48,9 @@ interface FormEntrySelect extends EntryBase {
 }
 
 type GenericFormItem =
-  | (FormEntry & { input: HTMLInputElement; el: HTMLDivElement })
-  | (FormEntryTextArea & { input: HTMLTextAreaElement; el: HTMLDivElement })
-  | (FormEntrySelect & { input: HTMLSelectElement; el: HTMLDivElement });
+  | (FormEntry & { el: HTMLDivElement })
+  | (FormEntryTextArea & { el: HTMLDivElement })
+  | (FormEntrySelect & { el: HTMLDivElement });
 
 export class SmartForm {
   /** @internal */
@@ -78,20 +73,18 @@ export class SmartForm {
         switch (o.type) {
           case 'string':
             {
-              const wrapperEl = div('tenilla-smart-form-item');
-              const labelEl = h('label', 'tenilla-smart-form-item-label', o.label).attr('for', id);
-              const inputEl = input('tenilla-smart-form-input').attr('type', 'text').attr('id', id);
-              if (o.value !== undefined) inputEl.value = String(o.value);
-              wrapperEl.child(labelEl, inputEl);
+              let inputEl: HTMLInputElement;
               rowData.push({
                 ...o,
-                el: wrapperEl,
-                input: inputEl,
+                el: div('tenilla-smart-form-item').child(
+                  label('tenilla-smart-form-item-label', o.label).attr('for', id),
+                  (inputEl = input('tenilla-smart-form-input').attrs({ id, value: o.value })),
+                ),
                 get value() {
-                  return (this.input as HTMLInputElement).value;
+                  return inputEl.value;
                 },
                 set value(v: string) {
-                  (this.input as HTMLInputElement).value = v ?? '';
+                  inputEl.value = v;
                 },
               });
             }
@@ -99,22 +92,20 @@ export class SmartForm {
 
           case 'number':
             {
-              const wrapperEl = div('tenilla-smart-form-item');
-              const labelEl = h('label', 'tenilla-smart-form-item-label', o.label).attr('for', id);
-              const inputEl = input('tenilla-smart-form-input')
-                .attr('type', 'number')
-                .attr('id', id);
-              if (o.value !== undefined) inputEl.valueAsNumber = o.value as number;
-              wrapperEl.child(labelEl, inputEl);
+              let inputEl: HTMLInputElement;
               rowData.push({
                 ...o,
-                el: wrapperEl,
-                input: inputEl,
+                el: div('tenilla-smart-form-item').child(
+                  label('tenilla-smart-form-item-label', o.label).attr('for', id),
+                  (inputEl = input('tenilla-smart-form-input')
+                    .attr('type', 'number')
+                    .attrs({ id, value: o.value })),
+                ),
                 get value() {
-                  return (this.input as HTMLInputElement).valueAsNumber;
+                  return inputEl.valueAsNumber;
                 },
                 set value(v: number) {
-                  (this.input as HTMLInputElement).valueAsNumber = v ?? 0;
+                  inputEl.valueAsNumber = v;
                 },
               });
             }
@@ -122,194 +113,75 @@ export class SmartForm {
 
           case 'textarea':
             {
-              const wrapperEl = div('tenilla-smart-form-item');
-              const labelEl = h('label', 'tenilla-smart-form-item-label', o.label).attr('for', id);
-              const textareaEl = h('textarea', 'tenilla-smart-form-textarea').attr('id', id);
-              if (o.value !== undefined) textareaEl.value = o.value;
-              wrapperEl.child(labelEl, textareaEl);
+              let inputEl: HTMLTextAreaElement;
               rowData.push({
                 ...o,
-                el: wrapperEl,
-                input: textareaEl,
+                el: div('tenilla-smart-form-item').child(
+                  label('tenilla-smart-form-item-label', o.label).attr('for', id),
+                  (inputEl = h('textarea', 'tenilla-smart-form-textarea').attrs({
+                    id,
+                    value: o.value,
+                  })),
+                ),
                 get value() {
-                  return (this.input as HTMLTextAreaElement).value;
+                  return inputEl.value;
                 },
                 set value(v: string) {
-                  (this.input as HTMLTextAreaElement).value = v ?? '';
+                  inputEl.value = v;
                 },
               });
             }
             break;
-
-          case 'select':
-            {
-              const wrapperEl = div('tenilla-smart-form-item');
-              const labelEl = h('label', 'tenilla-smart-form-item-label', o.label).attr('for', id);
-              const selectEl = h('select', 'tenilla-smart-form-select').attr('id', id);
-              const optionEls = o.options.map((opt) =>
-                h('option', '', opt.label).attr('value', opt.value),
-              );
-              selectEl.child(...optionEls);
-              if (o.value !== undefined) selectEl.value = o.value;
-              wrapperEl.child(labelEl, selectEl);
-              rowData.push({
-                ...o,
-                el: wrapperEl,
-                input: selectEl,
-                get value() {
-                  return (this.input as HTMLSelectElement).value;
-                },
-                set value(v: any) {
-                  (this.input as HTMLSelectElement).value = v ?? '';
-                },
-              });
-            }
-            break;
-
           case 'boolean':
             {
-              const wrapperEl = div('tenilla-smart-form-checkbox-wrapper');
-              const inputEl = input().attr('type', 'checkbox').attr('id', id);
-              if (o.value !== undefined) inputEl.checked = o.value as boolean;
-              const labelEl = h('label', 'tenilla-smart-form-checkbox-label', o.label).attr(
-                'for',
-                id,
-              );
-              wrapperEl.child(inputEl, labelEl);
+              let inputEl: HTMLInputElement;
               rowData.push({
                 ...o,
-                el: wrapperEl,
-                input: inputEl,
+                el: div('tenilla-smart-form-checkbox-wrapper').child(
+                  (inputEl = input().attrs({ type: 'checkbox', id, checked: o.value })),
+                  label('tenilla-smart-form-checkbox-label', o.label).attr('for', id),
+                ),
                 get value() {
-                  return (this.input as HTMLInputElement).checked;
+                  return inputEl.checked;
                 },
                 set value(v: boolean) {
-                  (this.input as HTMLInputElement).checked = v;
+                  inputEl.checked = v;
+                },
+              });
+            }
+            break;
+          case 'select':
+            {
+              let value: any = o.value;
+
+              const options: HTMLOptionElement[] = o.options.map((opt, idx) =>
+                option(idx, opt.label, value === opt.value).on('click', () => {
+                  value = opt.value;
+                  options.forEach((v) => (v.selected = false));
+                  options[idx].selected = true;
+                }),
+              );
+              rowData.push({
+                ...o,
+                el: div('tenilla-smart-form-item').child(
+                  label('tenilla-smart-form-item-label', o.label).attr('for', id),
+                  h('select', 'tenilla-smart-form-select')
+                    .attrs({ id })
+                    .child(...options),
+                ),
+                get value() {
+                  return value;
+                },
+                set value(v: any) {
+                  value = v;
                 },
               });
             }
             break;
 
-          case 'string-array':
-            {
-              const containerEl = div('tenilla-smart-form-item');
-              const labelEl = h('label', 'tenilla-smart-form-array-label', o.label);
-              const innerWrapper = div('tenilla-smart-form-array-wrapper');
-              const itemsEl = div('tenilla-smart-form-array-items');
-              const addBtnEl = button('btn btn-primary tenilla-smart-form-add-btn', '+ Add');
-              const items: HTMLInputElement[] = [];
-              const refreshItems = () => {
-                itemsEl.innerHTML = '';
-                for (const item of items) {
-                  const itemRow = div('tenilla-smart-form-array-item');
-                  const itemEl = input().attr('type', 'text');
-                  itemEl.value = (item as HTMLInputElement).value;
-                  const removeBtnEl = button('btn btn-danger btn-sm', '×');
-                  removeBtnEl.on('click', () => {
-                    const idx = items.indexOf(item);
-                    if (idx > -1) items.splice(idx, 1);
-                    refreshItems();
-                  });
-                  itemRow.child(itemEl, removeBtnEl);
-                  itemsEl.child(itemRow);
-                }
-              };
-              addBtnEl.on('click', () => {
-                const newItem = input().attr('type', 'text');
-                items.push(newItem);
-                refreshItems();
-              });
-              innerWrapper.child(itemsEl, addBtnEl);
-              containerEl.child(labelEl, innerWrapper);
-              if (o.value !== undefined && Array.isArray(o.value)) {
-                for (const val of o.value) {
-                  const item = input().attr('type', 'text');
-                  item.value = String(val);
-                  items.push(item);
-                }
-              }
-              refreshItems();
-              rowData.push({
-                ...o,
-                el: containerEl,
-                input: null as any,
-                get value() {
-                  return items.map((item) => item.value);
-                },
-                set value(v: string[]) {
-                  items.length = 0;
-                  if (Array.isArray(v)) {
-                    for (const val of v) {
-                      const item = input().attr('type', 'text');
-                      item.value = String(val);
-                      items.push(item);
-                    }
-                  }
-                  refreshItems();
-                },
-              });
-            }
+          case 'checkboxes':
             break;
-
-          case 'number-array':
-            {
-              const containerEl = div('tenilla-smart-form-item');
-              const labelEl = h('label', 'tenilla-smart-form-array-label', o.label);
-              const innerWrapper = div('tenilla-smart-form-array-wrapper');
-              const itemsEl = div('tenilla-smart-form-array-items');
-              const addBtnEl = button('btn btn-primary tenilla-smart-form-add-btn', '+ Add');
-              const items: HTMLInputElement[] = [];
-              const refreshItems = () => {
-                itemsEl.innerHTML = '';
-                for (const item of items) {
-                  const itemRow = div('tenilla-smart-form-array-item');
-                  const itemEl = input().attr('type', 'number');
-                  itemEl.valueAsNumber = Number((item as HTMLInputElement).value);
-                  const removeBtnEl = button('btn btn-danger btn-sm', '×');
-                  removeBtnEl.on('click', () => {
-                    const idx = items.indexOf(item);
-                    if (idx > -1) items.splice(idx, 1);
-                    refreshItems();
-                  });
-                  itemRow.child(itemEl, removeBtnEl);
-                  itemsEl.child(itemRow);
-                }
-              };
-              addBtnEl.on('click', () => {
-                const newItem = input().attr('type', 'number');
-                items.push(newItem);
-                refreshItems();
-              });
-              innerWrapper.child(itemsEl, addBtnEl);
-              containerEl.child(labelEl, innerWrapper);
-              if (o.value !== undefined && Array.isArray(o.value)) {
-                for (const val of o.value) {
-                  const item = input().attr('type', 'number');
-                  item.valueAsNumber = Number(val);
-                  items.push(item);
-                }
-              }
-              refreshItems();
-              rowData.push({
-                ...o,
-                el: containerEl,
-                input: null as any,
-                get value() {
-                  return items.map((item) => item.valueAsNumber);
-                },
-                set value(v: number[]) {
-                  items.length = 0;
-                  if (Array.isArray(v)) {
-                    for (const val of v) {
-                      const item = input().attr('type', 'number');
-                      item.valueAsNumber = Number(val);
-                      items.push(item);
-                    }
-                  }
-                  refreshItems();
-                },
-              });
-            }
+          case 'radios':
             break;
 
           // TODO 这里要增加三种时间组件
