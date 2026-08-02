fffaafc9838958d7e1a39f20e1782804005716e4
aldia_mechdream <futami16237@gmail.com>
2026-08-02 22:57:06 +0800

    feat: 增加类约束，待完成

--- packages/components/src/common.ts
+++ packages/components/src/common.ts
@@ -1,5 +1,13 @@
-import { hAlias } from '@tenilla/core';
+import { hAlias, TenillaComponent } from '@tenilla/core';
 
 export const [button, span, li, select, ul, input, dialog, label, textarea] = hAlias(
   'button,span,li,select,ul,input,dialog,label,textarea',
 );
+
+// TODO 增加类约束
+export abstract class TenillaInputComponent extends TenillaComponent {
+  abstract get value(): any;
+  abstract set value(v: any);
+  abstract get disabled(): boolean;
+  abstract set disabled(v: boolean);
+}
