7104d1b03591b3860da8e85e3b27438ccc17f785
aldia_mechdream <futami16237@gmail.com>
2026-08-05 12:25:21 +0800

    feat button初步完成

--- packages/components/src/Button/Button.ts
+++ packages/components/src/Button/Button.ts
@@ -1,5 +1,11 @@
 import { h } from '@tenilla/core';
+import './Button.css';
 
-export function btn(className?: string, child?: any) {
-  return h('button', `${className ?? ''} tenilla-btn`, child);
+export function btn(child: any): HTMLButtonElement;
+export function btn(variant: string, child?: any): HTMLButtonElement;
+export function btn(...args: any[]) {
+  if (args.length === 1) {
+    return h('button', 'tenilla-btn', args[0]);
+  }
+  return h('button', `btn-${args[0]} tenilla-btn`, args[1]);
 }
