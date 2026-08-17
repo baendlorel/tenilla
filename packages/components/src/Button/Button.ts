a4664111b3b86c4a0917e1ccc905a514d3478f45
aldia_mechdream <futami16237@gmail.com>
2026-08-17 10:28:20 +0800

    feat btn增加onclick release  @tenilla/components@0.2.7

--- packages/components/src/Button/Button.ts
+++ packages/components/src/Button/Button.ts
@@ -3,12 +3,23 @@ import type { TenillaVariant } from '../common.js';
 import './Button.css';
 
 export function btn(child: any): HTMLButtonElement;
-export function btn(variant: TenillaVariant, child?: any): HTMLButtonElement;
+export function btn(
+  variant: TenillaVariant,
+  child: any,
+  onClick?: HTMLElementEventMap['click'],
+): HTMLButtonElement;
 export function btn(...args: any[]) {
-  if (args.length === 1) {
-    return h('button', 'tenilla-btn', args[0]);
+  switch (args.length) {
+    case 0:
+    case 1:
+      return h('button', 'tenilla-btn', args[0]);
+    case 2:
+      return h('button', `btn-${args[0]} tenilla-btn`, args[1]);
+    case 3:
+      return h('button', `btn-${args[0]} tenilla-btn`, args[1]).on('click', args[2]);
+    default:
+      throw new Error(`Invalid number of arguments for 'btn' function`);
   }
-  return h('button', `btn-${args[0]} tenilla-btn`, args[1]);
 }
 
 export interface ButtonProps {
